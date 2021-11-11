1321ec3130f0568cba295b6e6b855204c5386e5d
Doctorwu <wuzhouchun@qq.com>
2021-11-11 18:45:38 +0800

    ✨ feat(tenon-cms): component-tree

---
+++ packages/tenon-cms/src/components/operation/component-tree.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="tree-container">
+    <a-tree
+      blockNode
+      :fieldNames="{
+        key: 'id',
+        title: 'name',
+        children: 'children'
+      }"
+      :show-line="true"
+      :data="[store.getters['viewer/getTree']]"
+    />
+  </section>
+</template>
+<script setup lang="ts">
+import { useStore } from '../../store';
+
+const store = useStore();
+
+</script>
+<style lang="scss" scoped>
+</style>
--- packages/tenon-cms/src/components/operation/index.vue
+++ packages/tenon-cms/src/components/operation/index.vue
@@ -6,16 +6,26 @@
       mode="horizontal"
       :ellipsis="false"
       :default-selected-keys="['materials']"
+      @menu-item-click="handleClickMenuItem"
     >
       <a-menu-item style="height: 30px;" key="materials">物料库</a-menu-item>
       <a-menu-item key="component-tree">组件树</a-menu-item>
     </a-menu>
-    <Materials></Materials>
+
+    <Materials v-if="key === 'materials'"></Materials>
+    <ComponentTree v-if="key === 'component-tree'"></ComponentTree>
   </section>
 </template>
 <script setup lang="ts">
 import Materials from './materials.vue';
+import { ref } from 'vue';
+import ComponentTree from './component-tree.vue';
 
+const key = ref('materials');
+
+const handleClickMenuItem = (activeKey: string) => {
+  key.value = activeKey;
+};
 </script>
 <style lang="scss" scoped>
 .wrapper {
