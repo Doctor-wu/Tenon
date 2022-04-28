07f1f11e8e0590e0a38d054c8312f5b811f7b5b3
doctorwu <doctorwu@tencent.com>
2022-04-28 20:23:00 +0800

    ✨ feat(tenon-materials): 文本域和时间选择器组件

--- packages/tenon-materials/components/web/Form/TextArea/TextArea.ts
+++ packages/tenon-materials/components/web/Form/TextArea/TextArea.ts
@@ -4,24 +4,28 @@ export default (lifeCycle, props, ctx, tenonComp) => {
     onMounted, onUpdated, onBeforeUnmount, onBeforeMount
   } = lifeCycle;
 
+  if (!tenonComp.propsBinding.hasBinding('textareaConfig', 'model-value')) {
+    tenonComp.propsBinding.addBinding('textareaConfig', 'model-value', '$comp.states.textareaValue');
+  }
   onMounted(() => {
     // console.log(lifeCycle, props, ctx, tenonComp);
+    tenonComp.eventCalledHook.onCalled((eventName, ...args) => {
+      if (eventName === "onInput") {
+        tenonComp.states.textareaValue = args[0];
+
+        const bindingExpression = tenonComp.propsBinding.getBinding('textareaConfig', 'model-value');
+        const value = args[0];
+        tenonComp.exec(`${bindingExpression} = ${JSON.stringify(value)}`);
+      }
+      if (eventName === "onClear") {
+        const bindingExpression = tenonComp.propsBinding.getBinding('textareaConfig', 'model-value');
+        tenonComp.exec(`${bindingExpression} = ''`);
+      }
+    });
   });
 
-  const add = () => {
-    tenonComp.states.count.value++;
-  }
-
-  const subtract = () => {
-    tenonComp.states.count.value--;
-  }
-
   return {
-    count: {
-      value: 0,
-    },
+    textareaValue: '',
     author: 'Doctorwu',
-    add,
-    subtract,
   }
 }
--- packages/tenon-materials/components/web/Form/TimePicker/TimePicker.ts
+++ packages/tenon-materials/components/web/Form/TimePicker/TimePicker.ts
@@ -4,24 +4,29 @@ export default (lifeCycle, props, ctx, tenonComp) => {
     onMounted, onUpdated, onBeforeUnmount, onBeforeMount
   } = lifeCycle;
 
+  if (!tenonComp.propsBinding.hasBinding('timePickerConfig', 'model-value')) {
+    tenonComp.propsBinding.addBinding('timePickerConfig', 'model-value', '$comp.states.timeValue');
+  }
   onMounted(() => {
     // console.log(lifeCycle, props, ctx, tenonComp);
+    tenonComp.eventCalledHook.onCalled((eventName, ...args) => {
+      if (eventName === "onChange") {
+        const [value, date, dateString] = args;
+        tenonComp.states.timeValue = date;
+
+        const bindingExpression = tenonComp.propsBinding.getBinding('timePickerConfig', 'model-value');
+        tenonComp.exec(`${bindingExpression} = ${JSON.stringify(value)}`);
+      }
+      if (eventName === "onClear") {
+        const [value, date, dateString] = args;
+        const bindingExpression = tenonComp.propsBinding.getBinding('timePickerConfig', 'model-value');
+        tenonComp.exec(`${bindingExpression} = ${JSON.stringify(date)}`);
+      }
+    });
   });
 
-  const add = () => {
-    tenonComp.states.count.value++;
-  }
-
-  const subtract = () => {
-    tenonComp.states.count.value--;
-  }
-
   return {
-    count: {
-      value: 0,
-    },
+    timeValue: '',
     author: 'Doctorwu',
-    add,
-    subtract,
   }
 }
