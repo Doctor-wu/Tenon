2f22ced30fe99971f1b9a3c0cfb00d59f5650e49
doctorwu <doctorwu@tencent.com>
2022-05-04 21:53:15 +0800

    ✨ feat(tenon-flow): 生产环境构建链

--- packages/tenon-cms/scripts/build.ts
+++ packages/tenon-cms/scripts/build.ts
@@ -5,9 +5,7 @@ import { createClient } from './client';
 const client = createClient();
 
 function build() {
-  childprocess.execSync('node --max_old_space_size=40960  node_modules/vite/bin/vite.js build', {
-    stdio: 'inherit',
-  });
+  childprocess.execSync('node --max_old_space_size=40960  node_modules/vite/bin/vite.js build');
   setPhase(client, BuildPhaseName.TRANSFORM_CMS_DIST);
 }
 
