c125ff4ada8dce17b2813b60c4f3da2e5e3ee075
Doctorwu <wuzhouchun@qq.com>
2022-09-14 19:55:19 +0800

    🐳 chore(shared, root, tenon-cms): chores

--- packages/shared/src/di/types/service.interface.ts
+++ packages/shared/src/di/types/service.interface.ts
@@ -3,7 +3,7 @@ import { newable } from "@tenon/shared";
 export interface IService<T extends newable = newable> {
   name: string;
   loader: Loader<T>;
-  onLoad?: (feature: IService<T>) => void;
+  onLoad?: (feature: T) => void;
   instance?: T;
 }
 
