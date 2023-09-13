4f4fe77aaadfb7c89d1bff8ea533fa406e420993
doctorwu <doctorwu@tencent.com>
2023-09-13 11:11:00 +0800

    🐞 fix(tenon-editor): 修复 move 错位问题

--- apps/tenon-editor/src/core/model/mutations/tree-mutations/move.ts
+++ apps/tenon-editor/src/core/model/mutations/tree-mutations/move.ts
@@ -1,15 +1,16 @@
 import { RuntimeTreeNode } from "../../data-structure";
 import { MutationError } from "../../errors";
 import { BaseMutation } from "../base";
-import { InsertTreeNodeMutation } from "./insert";
 
-export class MoveTreeNodeMutation extends InsertTreeNodeMutation {
+export class MoveTreeNodeMutation extends BaseMutation {
   oldParent: RuntimeTreeNode;
   newParent: RuntimeTreeNode;
   oldIndex: number;
+  newIndex: number;
+  source: RuntimeTreeNode;
 
   constructor(newParent: RuntimeTreeNode, source: RuntimeTreeNode, index: number) {
-    super(newParent, source, index);
+    super();
     if (!source.parent) {
       throw new MutationError(
         "Cannot move a node that does not have a parent"
@@ -18,14 +19,32 @@ export class MoveTreeNodeMutation extends InsertTreeNodeMutation {
     this.oldParent = source.parent;
     this.newParent = newParent;
     this.oldIndex = source.parent.children.indexOf(source) ?? -1;
+    this.newIndex = index;
+    this.source = source;
   }
 
   handle(): void {
-    super.handle();
+    // remove from old parent
     this.oldParent.children.splice(this.oldIndex, 1);
+    // insert to new parent
+    if (this.oldParent === this.newParent) {
+      // if the old parent equals to the new parent
+      // and the new index is larger than the old index
+      // we need to minus 1 cause the old index is already removed
+      if (this.oldIndex < this.newIndex) {
+        this.newParent.children.splice(this.newIndex - 1, 0, this.source);
+      } else if (this.oldIndex > this.newIndex) {
+        this.newParent.children.splice(this.newIndex, 0, this.source);
+      }
+    } else {
+      // if the old parent is not equal to the new parent
+      // we just insert the node to the new parent
+      this.newParent.children.splice(this.newIndex, 0, this.source);
+      this.source.parent = this.newParent;
+    }
   }
 
   reverse(): BaseMutation {
-    return new InsertTreeNodeMutation(this.oldParent, this.source, this.oldIndex);
+    return new MoveTreeNodeMutation(this.oldParent, this.source, this.oldIndex);
   }
 }
